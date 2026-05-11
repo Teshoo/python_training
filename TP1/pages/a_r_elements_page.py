fd27b1b736d12c42fc7d86674589fd596a5494d4
Teshoo <12759671+Teshoo@users.noreply.github.com>
2026-05-11 16:01:39 +0200

    test: add test for a/r elements

---
+++ TP1/pages/a_r_elements_page.py
@@ -0,0 +1,35 @@
+from pages.base_page import BasePage
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.select import Select
+
+class ARElementsPage(BasePage):
+    URL: str = "https://the-internet.herokuapp.com/add_remove_elements/"
+    
+    ADD_ELEMENT_BUTTON = (By.CSS_SELECTOR, "button[onclick='addElement()']")
+    DELETE_ELEMENT_BUTTON = (By.CSS_SELECTOR, "button[onclick='deleteElement()']")
+    
+    def wait_add_element_button_to_be_visible(self):
+        try:
+            self.wait.until(
+                EC.visibility_of_element_located(self.ADD_ELEMENT_BUTTON)
+            ), "Add Element button not visible"
+        except Exception as e:
+            print(f"Erreur : {e}")
+         
+    def click_on_add_element_button(self):
+        self.get_add_element_button().click()
+        
+    def click_on_first_delete_element_button(self):
+        self.get_delete_element_buttons()[0].click()
+        
+    def delete_all_elements(self):
+        for e in enumerate(self.get_delete_element_buttons()):
+            e[1].click()
+            
+    def get_add_element_button(self):
+        return self.driver.find_element(*self.ADD_ELEMENT_BUTTON)       
+
+    def get_delete_element_buttons(self):
+        return self.driver.find_elements(*self.DELETE_ELEMENT_BUTTON)
+    
---
+++ TP1/tests/test_ar_elements.py
@@ -0,0 +1,66 @@
+from selenium import webdriver
+from pages.a_r_elements_page import ARElementsPage
+
+import logging
+from datetime import datetime
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s'
+)
+
+logger = logging.getLogger(__name__)
+driver = webdriver.Chrome()
+
+def test_ar_elements()-> None:    
+    logger.info("Test Add/Remove Elements")
+    try:
+        logger.info("Initializing...")
+        
+        page = ARElementsPage(driver)
+        
+        logger.info("Navigating to Add/Remove Elements page...")
+        page.open(page.URL)
+ 
+        page.wait_add_element_button_to_be_visible()
+        assert page.get_add_element_button() is not None, f"Add Element button should be visible"
+        logger.info("ASSERT: Add/Remove Elements page displayed")
+        
+        logger.info("Clicking on Add Element Button 3 times...")
+        for i in range(0,3):
+            page.click_on_add_element_button()
+        nb_delete_buttons = len(page.get_delete_element_buttons())
+        assert nb_delete_buttons == 3, f"There should be 3 Delete buttons, got {nb_delete_buttons}"
+        logger.info("ASSERT: 3 elements added")
+        
+        logger.info("Clicking on first Delete element button...")
+        page.click_on_first_delete_element_button()
+        nb_delete_buttons = len(page.get_delete_element_buttons())
+        assert nb_delete_buttons == 2, f"There should be 2 Delete buttons, got {nb_delete_buttons}"
+        logger.info("ASSERT: 2 elements left")
+        
+        logger.info("Deleting all elements...")
+        page.delete_all_elements()
+        nb_delete_buttons = len(page.get_delete_element_buttons())
+        assert nb_delete_buttons == 0, f"There should be 0 Delete buttons, got {nb_delete_buttons}"
+        logger.info("ASSERT: 0 elements left")
+        
+    except Exception as e:
+        logger.error(f"Error: {e}")
+        logger.error("Stack trace:", exc_info=True)
+        
+        if driver:
+            screenshot_name = f"screenshots/error_{datetime.now().strftime('%Y%m%d_%H%M%S')}.png"
+            driver.save_screenshot(screenshot_name)
+            logger.info(f" Screenshot sauvegardé: {screenshot_name}")
+
+    finally:
+        if driver:
+            logger.info("Closing WebDriver...")
+            driver.quit()
+            logger.info(" WebDriver closed")
+        
+        logger.info("Test ended")
+        
+if __name__ == "__main__":
+    test_ar_elements()
